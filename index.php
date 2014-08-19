b48b39e88316f6906c3d0e33891e182b6a757086
ttcat <ttcat0902@gmail.com>
2014-08-19 21:40:06 +0800

    nid

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 global $u;
 $q = ltrim($_SERVER['REQUEST_URI'], '/');
 $q = preg_replace('/[^a-zA-Z0-9]\//', '', $q);
-list($u, $f) = explode('/', $q, 2);
+list($u, $f, $nid) = explode('/', $q, 3);
 if(file_exists($f.'.inc')){
   include_once($f.'.inc');
   exit();
