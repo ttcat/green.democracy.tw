ef7c5d0d32cfb4de9388f4d2b88ff4a477d77619
ttcat <ttcat0902@gmail.com>
2014-08-20 06:38:48 +0800

    read api/timeline

--- js/main.js
+++ js/main.js
@@ -68,7 +68,7 @@ $(document).ready(function() {
 	    	var pathnames = window.location.pathname.split('/');
 	    	
 			if (history.pushState) {
-			    var newurl = window.location.protocol + "//" + window.location.host + pathnames[0] + '/issues/nid' + $(this).attr('hash-id');
+			    var newurl = window.location.protocol + "//" + window.location.host + pathnames[0] + '/issues/nid/' + $(this).attr('hash-id');
 			    window.history.pushState({path:newurl},'',newurl);
 			}	    
 	    
