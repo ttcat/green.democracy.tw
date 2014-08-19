12d075a5d86dcc890ee71dd879e14978fd88a59b
Jimmy Huang <jimmy@netivism.com.tw>
2014-08-19 16:15:28 +0800

    Another nginx fake 404.

--- index.php
+++ index.php
@@ -20,13 +20,10 @@ if(file_exists($f.'.inc')){
 </style>
 </head>
 <body>
-<blockquote>
-  <h2>Error 404 Not Found</h2>
-  <p>Our apologies for the temporary inconvenience. The requested URL was not found on this server.  We suggest you try one of the links below:
-  <ul>
-      <li><b>Verify url and typos</b> - The web page you were attempting to view may not exist or may have moved - try <em>checking the web address for typos</em>.<//li>
-      <li><b>E-mail us</b> - If you followed a link from somewhere, please let us know at <a href="mailto:webmaster@example.com">webmaster@example.com</a>. Tell us where you came from and what you were looking for, and we'll do our best to fix it.</li>
-  </ul>
-</blockquote>
+<div align="center">
+<h1>404 Not Found</h1>
+<hr>
+Nginx
+</div>
 </body>
 </html>
