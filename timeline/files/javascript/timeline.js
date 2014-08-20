4a49f9bd6fb87e713f284521e7144623cb16bb47
ttcat <ttcat0902@gmail.com>
2014-08-21 07:32:50 +0800

    timeline json change

--- timeline/files/javascript/timeline.js
+++ timeline/files/javascript/timeline.js
@@ -422,10 +422,6 @@ function Timeline(element, data, undefined) {
             next_image.fadeIn();
         }
 
-
-		addthis.toolbox("#timeline_fb_23");
-		addthis.button("#timeline_fb_23");
-
         var timeout_id = setTimeout(function() {
             SELF._updateSlider(timeline_element, direction);
         }, timeline_element.data('speed'));
