a25fa1987217fd2de575ddfe418b132c9bd6ef43
ttcat <ttcat0902@gmail.com>
2014-08-25 03:08:40 +0800

    metadata

--- index.php
+++ index.php
@@ -16,6 +16,7 @@ list($u, $f, $key, $value) = explode('/', $q, 4);
 		if(is_numeric($value_array[0])) {	
 			$nid = filter_var($value_array[0], FILTER_SANITIZE_NUMBER_INT);
 			$query_this_page = $value_array[1]; //參數待處理 sql-injection
+			$current_url = $u.'/'.$f.'/nid/'.$nid;			
 		}
 	}
 	
@@ -24,6 +25,7 @@ list($u, $f, $key, $value) = explode('/', $q, 4);
 		if(is_numeric($key_array[0])) {	
 			$nid = filter_var($key_array[0], FILTER_SANITIZE_NUMBER_INT);
 			$query_this_page = $key_array[1]; //參數待處理 sql-injection
+			$current_url = $u.'/nid/'.$nid;
 		}
 		include_once('politician.inc');
 		exit();
@@ -33,6 +35,7 @@ list($u, $f, $key, $value) = explode('/', $q, 4);
 	 	$key_array = explode('?' ,$key_array, 2);
 	 	if($key_array[	0] == 'timeline') {
 	 		$timeline_page_query = $key_array[1]; //參數待處理 sql-injection
+			$current_url = $u.'/api/timeline';
 	 		include_once('api/timeline.inc');
 			exit();
 		}
@@ -42,11 +45,13 @@ list($u, $f, $key, $value) = explode('/', $q, 4);
 $f_array = explode('?' ,$f, 2);
 //預設 politician.inc 為首頁
 if($f_array[0] == null) {
+  $current_url = $u;
   include_once('politician.inc');
   exit();
 }
 
 if(file_exists($f_array[0].'.inc')){
+  $current_url = $u.'/'.$f_array[0];
   include_once($f_array[0].'.inc');
   exit();
 }
