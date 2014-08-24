3cb4ce548c3ca55e7bbc8d679a999dd333dfea11
ttcat <ttcat0902@gmail.com>
2014-08-25 03:14:31 +0800

    metadata

--- index.php
+++ index.php
@@ -57,23 +57,8 @@ if(file_exists($f_array[0].'.inc')){
   include_once($f_array[0].'.inc');
   exit();
 }
+
+  include_once('404.inc');
+  exit();
 ?>
-<html>
-<head>
-<meta http-equiv="content-type" content="text/html;charset=utf-8">
-<title>Error 404 Not Found</title>
-<style>
-<!--
-  body {font-family: arial,sans-serif}
-  img { border:none; }
-//-->
-</style>
-</head>
-<body>
-<div align="center">
-<h1>404 Not Found</h1>
-<hr>
-Nginx
-</div>
-</body>
-</html>
+
