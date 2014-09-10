581fdb5fa5c5b1e3c3c5fdc50e8d496e8f6d7ad8
ttcat <ttcat0902@gmail.com>
2014-09-11 02:53:33 +0800

    html5 youtube

--- timeline/files/javascript/timeline.js
+++ timeline/files/javascript/timeline.js
@@ -827,6 +827,7 @@ function Timeline(element, data, undefined) {
     this._processIframeQueue = function() {
         $(SELF._iframe_queue).each(function(index, queue) {
             //queue.element.removeClass('loading').html('<iframe frameborder="0" src="' + queue.url + '"></iframe>');
+			
 			queue.element.removeClass('loading').html('<object><param name="movie" value="' + queue.url + '"></param><embed src="' + queue.url + '" type="application/x-shockwave-flash"></embed></object>');
         });
     };
